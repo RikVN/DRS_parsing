6941f4c2703b6a0538bc49cd2077bef4e35a3927
RikVN <p266548@haytabo.let.rug.nl>
2019-01-10 10:43:00 +0100

    New version of spar for PMB release 2.2.0

--- parsing/spar.py
+++ parsing/spar.py
@@ -6,18 +6,22 @@ import sys
 
 ## Usage: python spar.py [FILE]
 if __name__ == '__main__':
-    # Baseline is DRS clause set for sentence "He smiled".
+    # Baseline is DRS clause set for sentence "Tom voted for himself".
+    # Based on the training set of PMB release 2.2.0 (English)
+	# p18/d1966 in the PMB
     baseline = \
 """
 b1 REF x1
+b1 Name x1 "tom"
 b1 male "n.02" x1
-b3 REF t1
-b3 TPR t1 "now"
-b3 time "n.08" t1
-k0 Agent e1 x1
-k0 REF e1
-k0 Time e1 t1
-k0 smile "v.01" e1
+b2 REF e1
+b2 REF t1
+b2 Agent e1 x1
+b2 TPR t1 "now"
+b2 Time e1 t1
+b2 time "n.08" t1
+b2 vote "v.01" e1
+b2 Theme e1 x1
 """
 
     for line in open(sys.argv[1], 'r'):
