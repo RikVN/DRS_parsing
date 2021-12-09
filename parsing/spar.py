9e75c8f24dee5be3d1b25faf30e60042b2a2c236
RikVN <p266548@let10.housing.rug.nl>
2021-12-09 15:39:56 +0100

    Add baseline DRS (spar) for 4.0.0

--- parsing/spar.py
+++ parsing/spar.py
@@ -6,25 +6,25 @@ import sys
 
 ## Usage: python spar.py [FILE]
 if __name__ == '__main__':
-    # Baseline is DRS clause set for sentence "He braggedd about it.".
-    # Based on the training set of PMB release 3.0.0 (English)
-	# p39/d2340 in the PMB
+    # Baseline is DRS clause set for sentence "I sacked him.".
+    # Based on the training set of PMB release 4.0.0 (English)
+    # p04/d2514 in the PMB
     baseline = \
 """
-b1 REF x1
-b1 PRESUPPOSITION b2
-b1 male "n.02" x1
-b2 REF e1
-b2 REF t1
-b2 Agent e1 x1
-b2 TPR t1 "now"
-b2 Time e1 t1
-b2 brag "v.01" e1
-b2 time "n.08" t1
-b2 Theme e1 x2
-b3 REF x2
-b3 PRESUPPOSITION b2
-b3 entity "n.01" x2
+b1 REF x1             % I [0...1]
+b1 EQU x1 "speaker"   % I [0...1]
+b1 person "n.01" x1   % I [0...1]
+b1 REF e1             % sacked [2...8]
+b1 REF t1             % sacked [2...8]
+b1 Agent e1 x1        % sacked [2...8]
+b1 TPR t1 "now"       % sacked [2...8]
+b1 Theme e1 x2        % sacked [2...8]
+b1 Time e1 t1         % sacked [2...8]
+b1 sack "v.02" e1     % sacked [2...8]
+b1 time "n.08" t1     % sacked [2...8]
+b2 REF x2             % him [9...12]
+b2 PRESUPPOSITION b1  % him [9...12]
+b2 male "n.02" x2     % him [9...12]
 """
 
     for line in open(sys.argv[1], 'r'):
